62d9f3397373b0fb7ee6cca8dfac5b405aabad81
Iarlori <ignacio.iarlori@aerolineas.com.ar>
2024-01-31 16:51:51 -0300

    Update 31/01/24

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,30 +5,29 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class PostController extends Controller
 {
     
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->except(['show','index']);
     }
 
     public function index(User $user)
     {
+        $posts = Post::where('user_id', $user->id)->paginate(20);
+
         return view('dashboard', [
-            'user'=> $user
+            'user'=> $user,
+            'posts'=> $posts
         ]);
     }
 
-    public function create(User $user)
+    public function create()
     {
-
-        $posts = Post::where('user_id', $user->id)->get();
-
-        return view('posts.create',[
-            'user'=> $user,
-            'posts'=> $posts
+        return view('posts.create',[   
         ]);
     }
 
@@ -63,6 +62,32 @@ class PostController extends Controller
             'imagen' => $request->imagen,
             'user_id' => auth()->user()->id
         ]);
+
+        return redirect()->route('posts.index', auth()->user()->username);
+    }
+
+
+    public function show(User $user, Post $post)
+    {
+        return view('posts.show', [
+            'post' => $post,
+            'user' => $user
+        ]);
+    }
+
+
+    public function destroy(Post $post)
+    {   
+        //Eliminar Posteo
+        $this->authorize('delete', $post);
+        $post->delete();
+
+        //Eliminar la Imagen
+        $imagen_path = public_path('uploads/' . $post->imagen);
+
+        if(File::exists($imagen_path)) {
+            unlink($imagen_path);
+        }
 
         return redirect()->route('posts.index', auth()->user()->username);
     }
